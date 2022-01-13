dbd6cc705ea0810f9fad43979901df7cdb207d97
Daniela Estrada <daniela.es.ib@gmail.com>
2022-01-12 21:44:54 -0600

    add buttons

--- App.js
+++ App.js
@@ -1,27 +1,40 @@
-import React from 'react'
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet } from 'react-native';
 
-import HomeScreen from './src/screens/HomeScreen'
-import DetailsScreen from './src/screens/DetailsScreen'
-import ImageScreen from './src/screens/ImageScreen'
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-const Stack = createStackNavigator()
+import HomeScreen from "./src/screens/HomeScreen";
+import DetailsScreen from "./src/screens/DetailsScreen";
+import ImageScreen from "./src/screens/ImageScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='App_to_Home'>
-        {/* screens */}
-        <Stack.Screen name='App_to_Home' component={HomeScreen} />
-        <Stack.Screen name='Home_to_Details' component={DetailsScreen} />
-        <Stack.Screen name='Details_to_Details' component={DetailsScreen} />
-        <Stack.Screen name='BigImageView' component={ImageScreen} />
+      <Stack.Navigator
+        initialRouteName="App_to_Home">
+        <Stack.Screen
+          name="App_to_Home"
+          component={HomeScreen}
+        />
+        <Stack.Screen
+          name="Home_to_Details"
+          component={DetailsScreen}
+        />
+        <Stack.Screen
+          name="Details_to_Details"
+          component={DetailsScreen}
+        />
+        <Stack.Screen
+          name="BigImageView"
+          component={ImageScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -31,4 +44,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-})
+});
