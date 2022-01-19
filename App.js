7ca86c0701693729ef3443299055fdfb9352da4d
Daniela Estrada <daniela.es.ib@gmail.com>
2022-01-18 21:02:19 -0600

    adding styles

--- App.js
+++ App.js
@@ -1,40 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet } from 'react-native'
 
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
-import HomeScreen from "./src/screens/HomeScreen";
-import DetailsScreen from "./src/screens/DetailsScreen";
-import ImageScreen from "./src/screens/ImageScreen";
+import HomeScreen from './src/screens/HomeScreen'
+import DetailsScreen from './src/screens/DetailsScreen'
+import ImageScreen from './src/screens/ImageScreen'
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="App_to_Home">
+        initialRouteName='App_to_Home'
+        screenOptions={{
+          headerTintColor: 'white',
+          headerStyle: { backgroundColor: 'orange' },
+        }}
+      >
         <Stack.Screen
-          name="App_to_Home"
+          name='App_to_Home'
           component={HomeScreen}
+          options={{
+            title: 'Movie Time',
+            headerTitleStyle: { fontWeight: 'bold', fontSize: 30 },
+          }}
         />
         <Stack.Screen
-          name="Home_to_Details"
+          name='Home_to_Details'
           component={DetailsScreen}
+          options={({ route }) => ({ title: route.params.movie.title })}
         />
         <Stack.Screen
-          name="Details_to_Details"
+          name='Details_to_Details'
           component={DetailsScreen}
+          options={({ route }) => ({ title: route.params.movie.title })}
         />
-        <Stack.Screen
-          name="BigImageView"
-          component={ImageScreen}
-        />
+        <Stack.Screen name='BigImageView' component={ImageScreen} />
       </Stack.Navigator>
     </NavigationContainer>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -44,4 +52,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,45 +1,84 @@
-import React from 'react';
-import { Button, View, Text, StyleSheet } from 'react-native';
+import React from 'react'
+import { Button, View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 
-const HomeScreen = ( {navigation} ) => {
-
-    return (
-      <View style={styles.mainView}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Star Wars"
-          onPress={() => {
-            navigation.navigate("Home_to_Details",{movie: {
-              title:        "Star Wars",
-              release:       1977,
-              screenNumber:  1
-            }} )}}/>
-        <Button
-          title="Black Panther"
-          onPress={() => {
-            navigation.navigate("Home_to_Details",{movie: {
-              title:          "Black Panther",
-              release:         2018,
-              screenNumber:    1
-            }} )}}/>
-        <Button
-          title="The Matrix"
-          onPress={() => {
-            navigation.navigate("Home_to_Details",{movie: {
-              title:          "The Matrix",
-              release:         1999,
-              screenNumber:    1
-            }} )}}/>
-      </View>
-    )
-};
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.mainView}>
+      <Text>Home Screen</Text>
+      <TouchableOpacity
+        style={styles.touchableView}
+        onPress={() => {
+          navigation.navigate('Home_to_Details', {
+            movie: {
+              title: 'Star Wars',
+              release: 1977,
+              screenNumber: 1,
+            },
+          })
+        }}
+      >
+        <View>
+          <Text style={styles.touchableText}>Star Wars</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.touchableView}
+        onPress={() => {
+          navigation.navigate('Home_to_Details', {
+            movie: {
+              title: 'Black Panther',
+              release: 2018,
+              screenNumber: 1,
+            },
+          })
+        }}
+      >
+        <View>
+          <Text style={styles.touchableText}>Black Panther</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.touchableView}
+        onPress={() => {
+          navigation.navigate('Home_to_Details', {
+            movie: {
+              title: 'The Matrix',
+              release: 1999,
+              screenNumber: 1,
+            },
+          })
+        }}
+      >
+        <View>
+          <Text style={styles.touchableText}>The Matrix</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
   mainView: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  }
-});
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  touchableView: {
+    marginBottom: 30,
+    width: 150,
+    height: 50,
+    alignItems: 'center',
+    backgroundColor: 'orange',
+    borderWidth: 5,
+    borderRadius: 10,
+  },
+  touchableText: {
+    textAlign: 'center',
+    paddingTop: 5,
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+})
 
-export default HomeScreen;
+export default HomeScreen
