d789c80cd3e7e08e48130e1e7b8aaf326d17fc56
Daniela Estrada <daniela.es.ib@gmail.com>
2022-01-26 22:38:19 -0600

    react native with expo finish

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, View, Text, StyleSheet } from 'react-native'
 
 const DetailsScreen = ({ navigation, route }) => {
-  console.log(route)
   const movie = route.params.movie
+  const [movieDetails, setMovieDetails] = useState(null)
   useEffect(() => {
     const xhr = new XMLHttpRequest()
     xhr.open(
@@ -12,11 +12,25 @@ const DetailsScreen = ({ navigation, route }) => {
     )
     xhr.send()
     xhr.onload = () => {
-      console.log(xhr.responseText)
+      if (xhr.status == 200) {
+        let response = JSON.parse(xhr.response)
+        setMovieDetails(response)
+      } else {
+        console.log(`HTTP Request Failed ${xhr.status}`)
+      }
+    }
+    xhr.onerror = () => {
+      console.log('Error')
     }
   }, [])
 
-  return <View style={styles.mainView}></View>
+  return (
+    <View style={styles.mainView}>
+      <Text>{movieDetails?.Title}</Text>
+      <Text>{movieDetails?.Released}</Text>
+      <Text>{movieDetails?.Plot}</Text>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
