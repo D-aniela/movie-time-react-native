30f36b9d1cff567d8bcf64411c1d3c9f8a03a0db
Daniela Estrada <daniela.es.ib@gmail.com>
2022-01-26 22:24:01 -0600

    api url json data

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -1,37 +1,30 @@
-import React from 'react';
-import { Button, View, Text, StyleSheet } from 'react-native';
+import React, { useEffect } from 'react'
+import { Button, View, Text, StyleSheet } from 'react-native'
 
-const DetailsScreen = ( {navigation, route} ) => {
-    console.log(route);
-    const movie = route.params.movie;
-    
-    return (
-      <View style={styles.mainView}>
-        <Text style={{fontSize: 20}}>{movie.title} ({movie.release})</Text>
-        <Text style={{fontSize: 100}} >{movie.screenNumber}</Text>
-        <Button
-          title="Go to Image"
-          onPress={() => {navigation.navigate("BigImageView")}}/>
-        <Button
-          title="More Details"
-          onPress={() => {
-            movie.screenNumber = movie.screenNumber + 1;
-            console.log(movie);
-            navigation.push("Details_to_Details",
-                            {movie: movie} )}}/>
-        <Button
-          title="Go Back to Home"
-          onPress={() => {navigation.popToTop()}}/>
-      </View>
+const DetailsScreen = ({ navigation, route }) => {
+  console.log(route)
+  const movie = route.params.movie
+  useEffect(() => {
+    const xhr = new XMLHttpRequest()
+    xhr.open(
+      'GET',
+      'http://www.omdbapi.com/?apikey=f2803024&t=Star+Wars&y=1977',
     )
-};
+    xhr.send()
+    xhr.onload = () => {
+      console.log(xhr.responseText)
+    }
+  }, [])
+
+  return <View style={styles.mainView}></View>
+}
 
 const styles = StyleSheet.create({
   mainView: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  }
-});
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
 
-export default DetailsScreen;
+export default DetailsScreen
